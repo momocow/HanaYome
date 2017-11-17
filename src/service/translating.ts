76c9395b464f458a571ce4d9bdbd9231dd09b0ae
MomoCow <momocow.me@gmail.com>
2017-11-17 17:46:45 +0800

    [skip ci] Fix language file problem, will test on Ubuntu VM then rebuild

--- src/service/translating.ts
+++ src/service/translating.ts
@@ -5,12 +5,19 @@ import * as globals from '../globals'
 import * as dict from '../util/Dictionary'
 
 import { appLog } from './logging'
+import { appConfig } from './configuring'
 
 let currentLocale: string, currentLangFile: string, currentDict: dict.Dictionary
 
 export function initTranslating(): void {
   appLog.debug('translating#initTranslating()')
-  use(locale.sync())
+
+  let userLang = appConfig.get('hanayome.lang')
+  if (!userLang) {
+    userLang = locale.sync()
+  }
+
+  use(userLang)
 }
 
 export function use(locale: string): void {
@@ -20,7 +27,15 @@ export function use(locale: string): void {
   currentLangFile = path.join(globals.ASSETS_PATH, 'lang', `${locale}.lang.cson`)
 
   appLog.info(`Using language file: ${currentLangFile}`)
-  currentDict = dict.Dictionary.getDict(currentLocale, currentLangFile)
+
+  try {
+    currentDict = dict.Dictionary.getDict(currentLocale, currentLangFile)
+  } catch (e) {
+    appLog.warn(`Lang file for ${locale} is not found.`)
+    appLog.warn(e)
+
+    use('zh_TW')
+  }
 }
 
 export function translate(key: string, namedFormattingArgs?: string | object, ...formattingArgs: any[]): string {
